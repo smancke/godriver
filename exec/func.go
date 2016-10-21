55c80027f019cb39552e5413c2725798de8e85ba
Christoph Borgolte <christoph.borgolte@bayard-consulting.com>
2016-10-21 15:14:16 +0200

    Removed unused code.

--- exec/func.go
+++ exec/func.go
@@ -1,9 +1,8 @@
 package exec
 
 type FuncExec struct {
-	f       func() error
-	name    string
-	retries uint
+	f    func() error
+	name string
 }
 
 func F(name string, f func() error) *FuncExec {
@@ -19,7 +18,4 @@ func (s *FuncExec) String(cntx Context) string {
 
 func (s *FuncExec) Exec(cntx Context) error {
 	return s.f()
-}
-func (s *FuncExec) Retries() uint {
-	return s.retries
 }
